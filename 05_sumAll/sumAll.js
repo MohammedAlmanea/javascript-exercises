6417f8d23e110daffc138a71d6690f64ba421cad
MohammedAlmanea <MohammedAlmanea@outlook.com>
2023-01-18 21:29:07 +0300

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    typeof num1 !== 'number' ||
+    typeof num2 !== 'number'
+  )
+    return 'ERROR';
+  let total = 0;
+  let largestNum;
+  let smallestNum;
+  if (num1 > num2) {
+    largestNum = num1;
+    smallestNum = num2;
+  } else {
+    largestNum = num2;
+    smallestNum = num1;
+  }
+  for (let i = largestNum; i != smallestNum - 1; i--) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
